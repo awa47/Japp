6473a36c6ae5a8fd12f45ef8324c767656796e18
usr2 <usr@cig.com>
2019-04-24 12:39:38 +0530

    bfggh

--- src/Japp1.java
+++ src/Japp1.java
@@ -2,8 +2,9 @@
 public class Japp1 {
      public static void main(String[] args) {
     	 
-    	 System.out.println("Hello git");
+     System.out.println("Hello git");
          System.out.println("line ws2");
+         System.out.println("line ws4");
      
      }
 }
